9fc469e730e8c5643d0b8807fff3b2252e31b168
JwrenKnight <116036240+JwrenKnight@users.noreply.github.com>
2022-10-17 20:08:43 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,7 +1,7 @@
 //Rule 00. Input Validation and Data Sanitization (IDS) IDS03-J
 
 if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + username);
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
 } else {
-  logger.severe("User login failed for: " + username);
+  logger.severe("User login failed for: " + sanitizeUser(username));
 }
