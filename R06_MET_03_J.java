1c20f480b24969616935eefe4993f95242ece4be
JwrenKnight <116036240+JwrenKnight@users.noreply.github.com>
2022-10-17 20:16:06 -0400

    Update R06_MET_03_J.java

--- R06_MET_03_J.java
+++ R06_MET_03_J.java
@@ -1,7 +1,7 @@
 //MET03-J. Methods that perform a security check must be declared private or final
 
 
-public void readSensitiveFile() {
+public final void readSensitiveFile() {
   try {
     SecurityManager sm = System.getSecurityManager();
     if (sm != null) {  // Check for permission to read file
